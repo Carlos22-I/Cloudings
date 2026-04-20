1adb4e1de2ad002d83e796bf9dce2b357692a003
Carlos22-I <3090cardani@gmail.com>
2026-04-19 21:17:04 -0500

    segundo commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,10 +4,12 @@ import { About } from './components/About';
 import { Plans } from './components/Plans';
 import { Clients } from './components/Clients';
 import { Testimonials } from './components/Testimonials';
+import { BrowserRouter } from 'react-router';
 import { Footer } from './components/Footer';
 
 export default function App() {
   return (
+    <BrowserRouter basename='/@figma/my-make-file'>
     <div className="min-h-screen bg-white">
       <Navbar />
       <div className="pt-16">
@@ -19,5 +21,6 @@ export default function App() {
         <Footer />
       </div>
     </div>
+    </BrowserRouter>
   );
 }
